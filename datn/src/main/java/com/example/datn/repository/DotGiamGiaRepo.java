9946786a52f0f5430eeddef795a7d752031fbc65
Nguyen Vu Long <longnv04102002@gmail.com>
2024-07-10 20:40:53 +0700

    no message

--- datn/src/main/java/com/example/datn/repository/DotGiamGiaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.datn.repository;
-
-import com.example.datn.entity.DotGiamGia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DotGiamGiaRepo extends JpaRepository<DotGiamGia, Long> {
-}
--- datn/src/main/java/com/example/datn/repository/HinhAnhRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.datn.repository;
-
-import com.example.datn.entity.HinhAnh;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HinhAnhRepo extends JpaRepository<HinhAnh, Long> {
-}
