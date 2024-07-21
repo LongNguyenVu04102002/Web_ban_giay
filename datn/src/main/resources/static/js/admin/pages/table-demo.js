043cde5980ce9d5a062078d8d820fd851a4314d8
Nguyen Vu Long <longnv04102002@gmail.com>
2024-07-21 21:18:26 +0700

    no message

--- datn/src/main/resources/static/js/admin/pages/table-demo.js
+++ datn/src/main/resources/static/js/admin/pages/table-demo.js
@@ -39,20 +39,14 @@ $(document).ready(function () {
         });
     };
 
-    filterColumn('#trangThaiFilter', 7);
+    filterColumn('#trangThaiFilter', 8);
     filterColumn('#loaiHoaDonFilter', 5);
     filterColumn('#maNhanVienFilter', 3);
     filterColumn('#sanPhamFilter', 3, ['#data-table-sp']);
     filterColumn('#loaiPhieuFilter', 2, ['#data-table-pgg']);
     filterColumn('#trangThaiPggFilter', 9, ['#data-table-pgg']);
     filterColumn('#gioiTinhAccFilter', 5, ['#data-table-account']);
-<<<<<<< HEAD
-    filterColumn('#trangThaiAccFilter', 7, ['#data-table-account']);
-    filterColumn('#trangThaiSpFilter', 10, ['#data-table-sp']);
-    filterColumn('#trangThaiSpctFilter', 9, ['#data-table-sp']);
-=======
     filterColumn('#trangThaiAccFilter', 6, ['#data-table-account']);
->>>>>>> KhachHang_LongFix
 
     const filterDateRange = () => {
         const fromDate = $('#fromDate').val();
