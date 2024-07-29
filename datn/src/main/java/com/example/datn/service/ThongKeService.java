993fab54c497e1ad8fb941d7bf03184640aab7a6
SilverLynel <101074214+SilverLynel@users.noreply.github.com>
2024-07-29 20:48:59 +0700

    thong ke dang lam do?

--- datn/src/main/java/com/example/datn/service/ThongKeService.java
+++ datn/src/main/java/com/example/datn/service/ThongKeService.java
@@ -1,4 +1,19 @@
 package com.example.datn.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ThongKeService {
+
+    List<String> getHoaDonToday();
+
+    Long countKhachHangByNgayTaoAndTrangThai(LocalDate ngayTao, int trangThai);
+
+    Long countKhachHangByYearMonthAndTrangThai(int year, int month, int trangThai);
+
+    Long countHoaDonByNgayTaoAndTrangThai(LocalDate ngayTao, int trangThai);
+
+    Long countHoaDonByYearMonthAndTrangThai(int year, int month, int trangThai);
+
+    List<Long> countHoaDonForCurrentWeek(int trangThai);
 }
