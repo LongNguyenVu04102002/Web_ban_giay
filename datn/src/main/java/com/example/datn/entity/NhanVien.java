783a7ddf76427c9d7237ea4dc85305841669cb8d
Nguyen Vu Long <longnv04102002@gmail.com>
2024-08-13 21:15:22 +0700

    no message

--- datn/src/main/java/com/example/datn/entity/NhanVien.java
+++ datn/src/main/java/com/example/datn/entity/NhanVien.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -43,6 +44,11 @@ public class NhanVien implements UserDetails {
     private String maNhanVien;
 
     @Column(name = "hoTen", length = 100)
+    @Size(max = 100, message = "Họ tên không được vượt quá 100 ký tự")
+    @Pattern(regexp = "^[\\p{L} \\s]*$", message = "Họ tên chỉ được chứa chữ cái và khoảng trắng")
+    @Pattern(regexp = "^(?!\\s).*$", message = "Họ tên không được bắt đầu bằng khoảng trắng")
+    @Pattern(regexp = ".*\\S$", message = "Họ tên không được kết thúc bằng khoảng trắng")
+    @Pattern(regexp = "^(?!.*\\s{2,}).*$", message = "Họ tên không được chứa nhiều khoảng trắng liên tiếp")
     private String hoTen;
 
     @Column(name = "gioiTinh")
@@ -50,12 +56,21 @@ public class NhanVien implements UserDetails {
 
     @Column(name = "ngaySinh")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past(message = "Ngày sinh phải là một ngày trong quá khứ")
+    @NotNull(message = "Không được để trống")
     private LocalDate ngaySinh;
 
     @Column(name = "sdt", length = 20)
+    @NotBlank(message = "Số điện thoại không được để trống")
+    @Size(max = 20, message = "Số điện thoại không được vượt quá 20 ký tự")
+    @Pattern(regexp = "^0\\d{9,19}$", message = "Số điện thoại phải bắt đầu bằng số 0 và chỉ chứa các chữ số")
     private String sdt;
 
     @Column(name = "email", length = 50)
+    @Size(max = 30, message = "Email không được vượt quá 30 ký tự")
+    @Email(message = "Email không hợp lệ")
+    @NotBlank(message = "Không được để trống")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Email không hợp lệ")
     private String email;
 
     @Column(name = "password")
