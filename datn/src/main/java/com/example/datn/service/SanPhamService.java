e5c7fb44bb67077d7de399605f43201e28e9ddb8
cacheplenbo <dqv2003nb@gmail.com>
2024-08-26 14:57:05 +0700

    no message

--- datn/src/main/java/com/example/datn/service/SanPhamService.java
+++ datn/src/main/java/com/example/datn/service/SanPhamService.java
@@ -1,6 +1,9 @@
 package com.example.datn.service;
 
+import com.example.datn.dto.SanPhamHomeDTO;
 import com.example.datn.entity.SanPham;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -16,5 +19,12 @@ public interface SanPhamService {
     List<SanPham> getSanPhamsByTrangThai(boolean trangThai);
 
     boolean findByTenAndChatLieu_ChatLieuIdAndCoGiay_CoGiayIdAndDayGiay_DayGiayIdAndDeGiay_DeGiayIdAndLotGiay_LotGiayIdAndMuiGiay_MuiGiayIdAndThuongHieu_ThuongHieuId(String ten, Long chatLieuId, Long coGiayId, Long dayGiayId, Long deGiayId, Long lotGiayId, Long muiGiayId, Long thuongHieuId);
+    Page<SanPhamHomeDTO> getSanPhamForHomePage(Pageable pageable) ;
+    Page<SanPhamHomeDTO> getSanPhamForShopPage(Long thuongHieuId, Long kichThuocId,
+                                               Long mauSacId,
+                                               String keyword,
+                                               Pageable pageable);
+
+
 
 }
